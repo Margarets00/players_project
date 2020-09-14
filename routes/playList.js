9e3bf990e4c88901cd7686d0e79e90a03c9cbb0d
unknown <gwangyeal@gmail.com>
2020-09-14 17:57:48 +0900

    0914_2

--- routes/playList.js
+++ routes/playList.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 router.get('/:id', function(req, res) {
     const findCode = req.params.id;
-    res.render('playList.ejs', {id : findCode});
+    res.render('./playList.ejs', {id : findCode});
   });
 
 module.exports = router;
