895b677de3cbdf833f5f48fc5052a01e1cde1a3f
GwangYeol Lee <gwangyeal@gmail.com>
2020-09-16 15:20:48 +0900

    0916_11

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/main', indexRouter);
+app.use('/', indexRouter);
 app.use('/playList', playRouter);
 
 // catch 404 and forward to error handler
